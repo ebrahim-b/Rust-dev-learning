f0ed8ba1a8ab83e019d5b6604c89d13a94749653
Ebrahim Barati Nasab <abrahim_barati@yahoo.com>
2023-09-11 21:30:26 +0330

    work with modules

---
+++ hello_rust/src/var.rs
@@ -0,0 +1,43 @@
+pub fn run() {
+    let _x:i32 = 12;
+    let c:char = 'ف';
+    println!("{}",c as i32);
+
+    let name:&str = "Ebrahim";
+    println!("{}",name);
+
+    let family:String = "Barati".to_string();
+    let full_name = name.to_string() + " " +  &family;
+
+    let full_name_2 = format!("{} {}",name,family);
+
+    println!("{}",full_name);
+    println!("{}",full_name_2);
+
+    let mut arr:[i32;3] = [2,95,63];
+    println!("{:?}",arr);
+
+    let arr_2 = [5;5];
+    println!("{:?}",arr_2);
+
+    let tup = ("ali",1542,12.5,true);
+    println!("{:?}",tup);
+
+    for n in arr.iter_mut() {
+        *n += 25;
+        println!("{}",n);
+    }
+    println!("{:?}",arr);
+
+    let mut v = Vec::new();
+    for i in 0..=10 {
+        v.push(i);
+    }
+
+    println!("{:?}",v);
+
+    let mat = [[2,5,6,7],[3,5,9,8]];
+
+    println!("{:?}",mat);
+
+}
